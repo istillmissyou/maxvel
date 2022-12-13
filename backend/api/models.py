40db387e49dd0f0ea9bdedd8d8d5435fb1c83593
Данил Штунь <danilshtun@yandex.ru>
2022-12-13 15:35:07 +0500

    in models add category, ingredient, position and ingredientinposition (test)

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,1 +1,51 @@
-from django.db import models
+from django.core.validators import MinValueValidator
+from django.db.models import (CASCADE, CharField, ForeignKey, ImageField,
+                              ManyToManyField, Model,
+                              PositiveSmallIntegerField, TextField)
+
+
+class Category(Model):
+    name = CharField(max_length=50)
+
+
+class Ingredient(Model):
+    name = CharField(max_length=50)
+    measurement_unit = CharField(max_length=30)
+
+
+class Position(Model):
+    name = CharField(max_length=50)
+    image = ImageField(upload_to='positions')
+    price = PositiveSmallIntegerField()
+    category = ManyToManyField(
+        Category,
+        related_name='positions',
+    )
+    amount = PositiveSmallIntegerField(
+        validators=(
+            MinValueValidator(1),
+        )
+    )
+    ingredients = ManyToManyField(
+        Ingredient,
+        related_name='positions',
+    )
+    text = TextField()
+
+
+class IngredientsInPosition(Model):
+    position = ForeignKey(
+        Position,
+        on_delete=CASCADE,
+        related_name='ingredient_position',
+    )
+    ingredient = ForeignKey(
+        Ingredient,
+        on_delete=CASCADE,
+        related_name='ingredient_position',
+    )
+    amount = PositiveSmallIntegerField(
+        validators=(
+            MinValueValidator(1),
+        )
+    )
